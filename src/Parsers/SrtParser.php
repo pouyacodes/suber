2dc779350738ee29719a8765545f57cba8127cca
Pouya Rezvani <thesirpr@gmail.com>
2021-09-28 11:46:12 +0330

    Better code logic for toTimeRepersent and toUnixTimestamp methods.

--- src/Parsers/SrtParser.php
+++ src/Parsers/SrtParser.php
@@ -9,61 +9,55 @@ use Suber\SubtitleCollection;
 
 class SrtParser implements ISubtitleType
 {
-    public function __construct()
-    {
-        date_default_timezone_set('UTC');
-    }
 
-    public function parse(string $text) : SubtitleCollection
+    public function parse(string $text): SubtitleCollection
     {
         // -------- remove the utf-8 BOM ----
-        $text = str_replace("\xEF\xBB\xBF", '', $text); 
+        $text = str_replace("\xEF\xBB\xBF", '', $text);
         $lines = explode(PHP_EOL, $text);
         $collection = new SubtitleCollection;
         $currentSubtitle = null;
-    
+
         foreach ($lines as $line) {
 
-            if($line) {
+            if ($line) {
 
-                if(ctype_digit($line)) {
+                if (ctype_digit($line)) {
                     $currentSubtitle = new Subtitle(0, 0, []);
                     $collection->add($currentSubtitle);
-                } else if(preg_match("/^(?<from>.+?) --> (?<to>.+?)$/", $line, $matches)) {
-                    $currentSubtitle && $currentSubtitle->setFrom( $this->toUnixTimestamp($matches['from']) );
-                    $currentSubtitle && $currentSubtitle->setTo( $this->toUnixTimestamp($matches['to']) );
-                }  else {
+                } else if (preg_match("/^(?<from>.+?) --> (?<to>.+?)$/", $line, $matches)) {
+                    $currentSubtitle && $currentSubtitle->setFrom($this->toUnixTimestamp($matches['from']));
+                    $currentSubtitle && $currentSubtitle->setTo($this->toUnixTimestamp($matches['to']));
+                } else {
                     $currentSubtitle && $currentSubtitle->setTexts($line);
                 }
-
             }
-            
         }
 
         return $collection;
     }
 
-    public function dumpSingle($subtitle) : string
+    public function dumpSingle($subtitle): string
     {
         $result = '';
-        if($subtitle instanceof Subtitle) {
+        if ($subtitle instanceof Subtitle) {
             $text = implode(PHP_EOL, $subtitle->getTexts());
-            $time = sprintf("%s --> %s", $this->toTimeRepersent($subtitle->getFrom()), $this->toTimeRepersent($subtitle->getTo()) );
+            $time = sprintf("%s --> %s", $this->toTimeRepersent($subtitle->getFrom()), $this->toTimeRepersent($subtitle->getTo()));
             $result = $time . PHP_EOL . $text . PHP_EOL;
         }
         return $result;
     }
 
-    public function dump($collection = []) : string
+    public function dump($collection = []): string
     {
         $subtitles = [];
 
         $counter = 1;
-        foreach($collection as $subtitle) {
+        foreach ($collection as $subtitle) {
 
             $result = $this->dumpSingle($subtitle);
             if ($result) {
-                $subtitles []= $counter . PHP_EOL . $result;
+                $subtitles[] = $counter . PHP_EOL . $result;
                 $counter++;
             }
         }
@@ -71,10 +65,10 @@ class SrtParser implements ISubtitleType
         return trim(implode(PHP_EOL, $subtitles));
     }
 
-    public function dumpGenerator($collection = []) : Generator
+    public function dumpGenerator($collection = []): Generator
     {
         $counter = 1;
-        foreach($collection as $subtitle) {
+        foreach ($collection as $subtitle) {
 
             $result = $this->dumpSingle($subtitle);
             if ($result) {
@@ -84,27 +78,42 @@ class SrtParser implements ISubtitleType
         }
     }
 
-    private function toUnixTimestamp(string $timeString) 
+    private function toUnixTimestamp(string $timeString)
     {
+        $timeString = str_replace(",", ":", $timeString);
         $timestamp = 0;
-        $units = [60, 3600, 86400, 2678400];
         $timeChunks = explode(':', $timeString);
-        $seconds = str_replace(',', '.', array_pop($timeChunks));
-        $timeChunks = array_reverse($timeChunks);
-
-        for ($i = 0; $i < count($timeChunks); $i++) { 
-            $timestamp += $timeChunks[$i] * $units[$i];
-        }
-
-        $timestamp += $seconds;
+        $timestamp = mktime($timeChunks[0] ?? 0, $timeChunks[1] ?? 0, $timeChunks[2] ?? 0, 1, 1, 1970);
+        $miliseconds = !empty($timeChunks[3]) ? $timeChunks[3] / 1000 : 0;
+        $timestamp += $miliseconds;
         return $timestamp;
     }
 
     private function toTimeRepersent(float $time)
     {
         $timeString = '';
-        $timeChunks = explode('.', $time);
-        $timeString = count($timeChunks) == 2 ? date('H:i:s,', $timeChunks[0]) . ($timeChunks[1] * 10 ** (3 - strlen($timeChunks[1]))) : date('H:i:s', $timeChunks[0]);
+        $seconds = (int) $time;
+        $miliseconds = $time - $seconds;
+        $miliseconds = $miliseconds ? substr($miliseconds, 2) : 0;
+
+        $timeUnits = [
+            (int) ($seconds / 3600),
+            (int) ($seconds % 3600 / 60),
+            (int) ($seconds % 60)
+        ];
+
+        $timeUnits = array_map(function ($timeUnit) {
+            if (strlen($timeUnit) == 1)
+                return '0' . $timeUnit;
+            return $timeUnit;
+        }, $timeUnits);
+
+
+        $timeString = implode(":", $timeUnits);
+
+        if ($miliseconds)
+            $timeString .= "," . $miliseconds * (10 ** (3 - strlen($miliseconds)));
+
         return $timeString;
     }
 }
